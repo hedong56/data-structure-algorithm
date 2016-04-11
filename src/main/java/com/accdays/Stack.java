e64fa8e80bf0e0f0e97e400da30d169e521189c9
hedong56 <867459110@qq.com>
2016-04-11 23:13:42 +0800

    栈

---
+++ src/main/java/com/accdays/Stack.java
@@ -0,0 +1,30 @@
+package com.accdays;
+
+/**
+ * 栈
+ * @author hedong
+ * @date 2016年4月10日 下午4:14:48
+ * @modifyNote
+ * @version 1.0
+ */
+public class Stack {
+	
+	int idx=0;
+	
+	char[] data=new char[10];
+	
+	//保存数据
+	public void push(char c){
+		synchronized (this) {
+			data[idx]=c;
+			idx++;
+		}
+	}
+
+	//取数据
+	public synchronized char pop(){
+		idx--;
+		return data[idx];
+	}
+	
+}
