65c6cca43e36732e412107b6aeed5107d2be24d9
hedong56 <867459110@qq.com>
2019-06-11 17:42:41 +0800

    修改过期方法

--- src/main/java/com/accdays/algorithm/sort/ComparableTest.java
+++ src/main/java/com/accdays/algorithm/sort/ComparableTest.java
@@ -3,8 +3,8 @@ package com.accdays.algorithm.sort;
 public class ComparableTest {
 
 	public static void main(String[] args) {
-		Integer a=new Integer(6);
-		Integer b=new Integer(4);
+		Integer a=Integer.valueOf(6);
+		Integer b=Integer.valueOf(4);
 		System.out.println(a.compareTo(b));
 	}
 
