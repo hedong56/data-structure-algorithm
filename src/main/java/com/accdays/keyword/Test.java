f1cd095288ce22a1f65bfd44ad193f0b32c74b6d
hedong56 <867459110@qq.com>
2019-06-12 15:43:00 +0800

    2. {}代码块、实例化属性字段 相同执行级别，按照代码顺序执行

--- src/main/java/com/accdays/keyword/Test.java
+++ src/main/java/com/accdays/keyword/Test.java
@@ -3,12 +3,20 @@ package com.accdays.keyword;
 /**
  * 执行顺序：
  *  1. 先 static 块
- *  2. 实例化 属性字段
+ *  2. {}代码块、实例化属性字段 相同执行级别，按照代码顺序执行
  *  3. 构造方法
  *  4. 先父类再子类
  */
 public class Test {
+
+    {
+        //代码域中定义的变量都是局部的，只有域中的代码可以调用。
+        String str="直接大括号";
+        System.out.println(str);
+    }
+
     Person person = new Person("Test");
+
     static{
         System.out.println("test static");
     }
