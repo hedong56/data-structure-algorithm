3ac79d73c4a62a231db13addba04a0d0ee471eac
hedong56 <867459110@qq.com>
2019-06-12 14:51:28 +0800

    插入排序

--- src/main/java/com/accdays/algorithm/sort/InsertSort.java
+++ src/main/java/com/accdays/algorithm/sort/InsertSort.java
@@ -13,7 +13,8 @@ public class InsertSort {
 
 	/**
 	 * 插入排序
-	 *
+	 * 关键：在前面已经排好序的序列中找到合适的插入位置
+	 * https://blog.csdn.net/zdp072/article/details/44221803
 	 * 依次比较，逐位后移
 	 *
 	 * 取一个标杆值，认为标杆值左边的数都是已经排好序的，右边的乱序。 从索引为1的数开始，把标杆值依次和左边的有序值进行比较，
@@ -28,18 +29,21 @@ public class InsertSort {
 	 */
 	static int[] insertSort(int[] arr) {
 
-		int left, right;
+		int left; //左面已经排好序的最大序号
+		int right;	//右边未排序的最小序号
 
 		for (right = 1; right < arr.length; right++) {
-			int temp = arr[right];
-			left = right;
-			while (left > 0 && temp < arr[left - 1]) {
-				//如果左边的相邻数比标杆值大，则将左边的数向右移一位
-				arr[left] = arr[left - 1];
+			int temp = arr[right];  //需要排序插入的元素
+			left = right-1;
+			while (left >= 0 && arr[left] > temp) {
+				//如果左边的数比标杆值大，则将左边的数向右移一位,向后移动一位
+				arr[left+1] = arr[left];
 				left--;
 			}
-			arr[left] = temp;
-			System.out.println(Arrays.toString(arr));
+			//左边的arr[left] 已经小于标杆值temp
+			//由于上面的循环最后减了一位，所以要加1
+			arr[left+1] = temp;
+//			System.out.println(Arrays.toString(arr));
 		}
 		return arr;
 	}
