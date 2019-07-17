cd167c6b02e99bab450e6dfab1c9f05672bc9dfd
hedong56 <867459110@qq.com>
2019-07-17 11:16:36 +0800

    runnable的lambda表达式方式实现

--- src/main/java/com/accdays/thread/ThreadImplments.java
+++ src/main/java/com/accdays/thread/ThreadImplments.java
@@ -3,8 +3,11 @@ package com.accdays.thread;
 public class ThreadImplments {
 
     public static void main(String[] args) {
+        //普通方式
 //        new Thread(new BusinessThread(),"t2").start();
 
+        //Lambda表达式方式
+        //@FunctionalInterface 只能有一个方法，(方法参数)->{方法体}
         new Thread(() -> System.out.println(Thread.currentThread().getName()),"t2").start();
 
 
