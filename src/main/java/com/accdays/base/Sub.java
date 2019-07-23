fff4973a072eb9137d102d6f253f5363aaa65656
hedong56 <867459110@qq.com>
2019-07-23 15:14:46 +0800

    Overload和Overrider

--- src/main/java/com/accdays/base/Sub.java
+++ src/main/java/com/accdays/base/Sub.java
@@ -10,9 +10,13 @@ package com.accdays.base;
  * @updateDate 2019/7/23 14:59
  **/
 public class Sub extends Super {
-    //方法的重载是通过参数的差异来区分
-    //方法的覆盖要求返回值，函数名，参数列表完全一样
-    public long getLength(){
+    //方法的重载是通过参数的差异来区分  Overload
+    //方法的覆盖要求返回值，函数名，参数列表完全一样 Overrider
+//    public long getLength(){
+//        return 5;
+//    }
+
+    public long getLength(int x){
         return 5;
     }
 
