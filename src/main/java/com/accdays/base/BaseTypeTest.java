b517990abdeb6853d6ca8d14069eea11efe6155b
hedong56 <867459110@qq.com>
2019-07-29 10:11:37 +0800

    基础类与封装类型转换

---
+++ src/main/java/com/accdays/base/BaseTypeTest.java
@@ -0,0 +1,34 @@
+package com.accdays.base;
+
+/**
+ * @author hedong
+ * @version 1.0
+ * @description
+ * @updateRemark
+ * @updateUser
+ * @createDate 2019/7/16 17:38
+ * @updateDate 2019/7/16 17:38
+ **/
+public class BaseTypeTest {
+
+
+    /**
+    * @description  能正常编译，不能运行。
+    * @param args :
+    * @return      void
+    * @author      hedong
+    * @date        2019/7/16 17:40
+    **/
+    public static void main(String[] args) {
+
+        int i=0;
+        int result=i>1?0:null;
+
+        System.out.println(result);
+
+    }
+
+
+
+
+}
